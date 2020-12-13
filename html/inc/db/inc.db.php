5c32f12fe869c68ac89f1f70a157d630d248ae1b
David <david.crittin@gmail.com>
2020-12-13 22:48:03 +0100

    Test localhost

--- html/inc/db/inc.db.php
+++ html/inc/db/inc.db.php
@@ -5,7 +5,7 @@ class Database
 
     private $_user;
     private $_passwd;
-    private $_dsn = "mysql:host=openvet.tk;dbname=openvet";
+    private $_dsn;
     private $_pdo;
 
     public function __construct()
@@ -14,6 +14,11 @@ class Database
         if (strlen($content) > 0) {
             $this->_user = explode("|", $content)[0];
             $this->_passwd = explode("|", $content)[1];
+            if($_SERVER["HTTP_HOST"] == "localhost") {
+                $this->_dsn = "mysql:host=openvet.tk;dbname=openvet";
+            } else {
+                $this->_dsn = "mysql:host=localhost;dbname=openvet";
+            }
             $this->_pdo = new PDO($this->_dsn, $this->_user, $this->_passwd);
         }
     }
