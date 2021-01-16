e02f896c6aaaad2fd4fec88fa84d2c8f79caaae5
Sylvain Meyer <sylvain.meyer@hes-so.ch>
2021-01-16 10:33:59 +0100

    Correction sur le filtrage à l'insertion des vétérinaires

--- html/inc/api/api.tealsearch.php
+++ html/inc/api/api.tealsearch.php
@@ -15,8 +15,8 @@ class ApiTelSearch
 
     public function insertAllVets(){
         // old key --> 343b8735ba6d3de3bd71269774e165f3
-        // old key --> 04e22ef45a6535ce713a000d0550bae4
-        $map_url = "https://tel.search.ch/api/?was=v%C3%A9t%C3%A9rinaire&wo=valais&maxnum=200&key=7608ea265ec632dbb844da39a40dfa6e";
+        // old key --> 7608ea265ec632dbb844da39a40dfa6e
+        $map_url = "https://tel.search.ch/api/?was=v%C3%A9t%C3%A9rinaire&wo=valais&maxnum=200&key=04e22ef45a6535ce713a000d0550bae4&lang=fr";
         //$map_url =  "../../data.xml";
 
         if (($response_xml_data = file_get_contents($map_url))===false){
@@ -32,19 +32,30 @@ class ApiTelSearch
             } else {
                 $database = new Database();
                 foreach ($data->entry as $entry){
-                    $isVet = false;
-                    $categories = $entry->xpath('tel:category');
+                    $isVet = 0;
 
-                    //echo $entry->title." | ";
-                    for($i = 0; $i < sizeof($categories); $i++){
-                        //echo strtoupper($categories[$i]).",(".is_int(strpos(strtoupper($categories[$i]), "TIER")).")";
-                        if(is_int(strpos(strtoupper($categories[$i]), "TIER"))){
-                            $isVet = true;
+                    // contrôle si l'occupation du vétérinaire contient "vét"
+                    if(is_int(strpos(strtoupper($entry->xpath('tel:occupation')[0]), "VéT"))){
+                        $isVet = 1;
+                    }
+
+                    // contrôle si le titre du vétérinaire contient "vét"
+                    if(is_int(strpos(strtoupper($entry->title), "VéT"))){
+                        $isVet = 1;
+                    }
+
+                    if($isVet==0){
+                        // contrôle si une des catégories du vétérinaire contient "vét"
+                        $categories = $entry->xpath('tel:category');
+                        for($i = 0; $i < sizeof($categories); $i++){
+                            if(is_int(strpos(strtoupper($categories[$i]), "VéT"))){
+                                $isVet = 1;
+                            }
                         }
                     }
-                    //echo "<br/><br/>";
 
-                    if($isVet){
+                    if($isVet==1){
+                        //echo $entry->title."<br/><br/>";
                         $email = "--";
                         if(sizeof($entry->xpath("tel:extra[@type='email']"))>0) {
                             $email = rtrim(($entry->xpath("tel:extra[@type='email']")[0]), '*');
